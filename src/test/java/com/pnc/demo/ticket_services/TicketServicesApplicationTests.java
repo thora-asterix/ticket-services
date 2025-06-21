64143b834684996dc69c55e2374ceb737be9fc62
Thora P <thora.asterix@gmail.com>
2025-06-21 17:21:21 -0500

    Initial Commit

---
+++ src/test/java/com/pnc/demo/ticket_services/TicketServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnc.demo.ticket_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
